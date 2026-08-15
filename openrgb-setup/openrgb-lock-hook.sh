b99d6163efb2f252890882eca1440f9469eb6483
newtox <futzy261@gmail.com>
2026-08-16 00:14:23 +0200

    Replace unreliable CLI profile loading with explicit per-device color commands

--- openrgb-setup/openrgb-lock-hook.sh
+++ openrgb-setup/openrgb-lock-hook.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+ORANGE="ff8000"
+
+turn_off() {
+    ~/.local/bin/OpenRGB.AppImage --device "ASUS ROG STRIX B850-A GAMING WIFI" --mode direct --color 000000
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Kraken V3 HyperSense" --mode direct --color 000000
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Basilisk V3" --mode direct --color 000000
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Goliathus Extended" --mode direct --color 000000
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Huntsman V2" --mode static --color 000000
+}
+
+turn_orange() {
+    ~/.local/bin/OpenRGB.AppImage --device "ASUS ROG STRIX B850-A GAMING WIFI" --mode direct --color $ORANGE
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Kraken V3 HyperSense" --mode direct --color $ORANGE
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Basilisk V3" --mode static --color $ORANGE
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Goliathus Extended" --mode static --color $ORANGE
+    ~/.local/bin/OpenRGB.AppImage --device "Razer Huntsman V2" --mode static --color $ORANGE
+}
+
+sleep 3
+turn_orange
+
 gdbus monitor -y -d org.freedesktop.login1 | while read -r line; do
     if echo "$line" | grep -q "LockedHint': <true>"; then
-        ~/.local/bin/OpenRGB.AppImage --device "ASUS ROG STRIX B850-A GAMING WIFI" --mode direct --color 000000
-        ~/.local/bin/OpenRGB.AppImage --device "Razer Kraken V3 HyperSense" --mode direct --color 000000
-        ~/.local/bin/OpenRGB.AppImage --device "Razer Basilisk V3" --mode direct --color 000000
-        ~/.local/bin/OpenRGB.AppImage --device "Razer Goliathus Extended" --mode direct --color 000000
-        ~/.local/bin/OpenRGB.AppImage --device "Razer Huntsman V2" --mode static --color 000000
+        turn_off
     elif echo "$line" | grep -q "LockedHint': <false>"; then
-        ~/.local/bin/OpenRGB.AppImage --profile "Static Orange Razer All"
+        turn_orange
     fi
 done
